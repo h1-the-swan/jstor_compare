69c41586b3753035a6e74d5c4aca174102bd83c0
h1-the-swan <jason.portenoy@gmail.com>
2017-02-04 09:04:39 -0800

    script to get size and depth data from tree file

--- get_size_and_depth_from_tree.py
+++ get_size_and_depth_from_tree.py
@@ -8,17 +8,21 @@ logging.basicConfig(format='%(asctime)s %(name)s.%(lineno)d %(levelname)s : %(me
         level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def get_size_counts(fname):
+def analyze_treefile(fname):
     """From the tree file, get the data needed to analyze
 
     :fname: filename of the tree file
-    :returns: counter of size: count
+    :returns: node_size_depth: list of tuples,
+                size_counts: counter of size: count,
+                depth_counts: counter of depth: count
 
     """
     start = time.time()
     logger.debug("Analyzing tree file...")
     i = 0
+    node_cluster = {}
     size_total = Counter()
+    cluster_depth = {}
     num_nodes = 0
     
 
@@ -29,12 +33,22 @@ def get_size_counts(fname):
                 continue
             line = line.strip().split(' ')
 
+            # Get paper ID
+            pid = int(line[2].strip('"'))
+
             # Get cluster
             cl = line[0]
             cl = cl.split(':')
 
+            # Get depth
+            depth = len(cl)
+
             belongs_to = ":".join(cl[:-1])
+            node_cluster[pid] = belongs_to
             size_total[belongs_to] += 1
+
+            if belongs_to not in cluster_depth:
+                cluster_depth[belongs_to] = depth
 
             num_nodes += 1
 
@@ -42,35 +56,74 @@ def get_size_counts(fname):
     for count in size_total.itervalues():
         size_counts[count] += 1
 
+    depth_counts = Counter()
+    for cl_name, depth in cluster_depth.iteritems():
+        depth_counts[depth] += 1
+
+    node_size_depth = []
+    for pid, belongs_to in node_cluster.iteritems():
+        cl_size = size_total[belongs_to]
+        cl_depth = cluster_depth[belongs_to]
+        row = (pid, cl_size, cl_depth)
+        node_size_depth.append(row)
+
 
     end = time.time()
     logger.debug("Done. Took {:2f} seconds.".format(end-start))
     logger.debug("Total number of nodes: {}.\n".format(num_nodes))
-    return size_counts
+    return node_size_depth, size_counts, depth_counts
 
 
-def output_csv(d, dirname, fname, extension='csv', sep=','):
+def output_csv(d, dirname, fname, extension='csv', sep=',', header=None):
     fname = os.path.join(dirname, "{}.{}".format(fname, extension))
     logger.debug("Writing to file: {}".format(fname))
     with open(fname, 'w') as outf:
-        for k, v in d.iteritems():
-            outf.write("{}{}{}\n".format(k, sep, v))
+        if header:
+            outf.write(sep.join(header))
+            outf.write("\n")
+
+        if isinstance(d, dict):
+            for k, v in d.iteritems():
+                outf.write("{}{}{}\n".format(k, sep, v))
+        elif isinstance(d, list):
+            for row in d:
+                row = [str(x) for x in row]
+                row = sep.join(row)
+                outf.write(row)
+                outf.write("\n")
+
+def output_node_size_depth(data, dirname, label):
+    header = ['pid', 'cl_size', 'cl_depth']
+    fname = label+"-node_size_depth"
+    output_csv(data, dirname, fname, header=header)
+
+def output_size_counts(data, dirname, label):
+    header = ['cl_size', 'num_clusters']
+    fname = label+"-size_counts"
+    output_csv(data, dirname, fname, header=header)
+
+def output_depth_counts(data, dirname, label):
+    header = ['cl_depth', 'num_clusters']
+    fname = label+"-depth_counts"
+    output_csv(data, dirname, fname, header=header)
 
 def main(args):
-    size_counts = get_size_counts(args.tree_fname)
+    node_size_depth, size_counts, depth_counts = analyze_treefile(args.tree_fname)
     outdir = args.outdir
     label = args.label
-    output_csv(size_counts, dirname=outdir, fname=label+"-size_counts")
+    output_node_size_depth(node_size_depth, outdir, label)
+    output_size_counts(size_counts, outdir, label)
+    output_depth_counts(depth_counts, outdir, label)
 
 if __name__ == "__main__":
     total_start = time.time()
     logger.info(" ".join(sys.argv))
     logger.info( '{:%Y-%m-%d %H:%M:%S}'.format(datetime.now()) )
     import argparse
-    parser = argparse.ArgumentParser(description="From a .tree file, get the distribution of number of nodes per cluster")
+    parser = argparse.ArgumentParser(description="From a .tree file, get three analysis files: 1) csv of node_name, size of its cluster, depth of its cluster (this will be a rather large file); 2) csv of cluster size, number of clusters of that size; 3) csv of cluster depth, number of clusters of that depth")
     parser.add_argument("tree_fname", type=str, help="input filename (.tree file)")
     parser.add_argument("-o", "--outdir", type=str, help="directory for the output", default=os.path.dirname(__file__))
-    parser.add_argument("--label", type=str, help="label for the output files--- e.g. 'EXAMPLE' will have output file 'EXAMPLE-size_counts.csv'")
+    parser.add_argument("--label", type=str, help="label for the output files--- e.g. 'EXAMPLE' will have output files 'EXAMPLE-size_and_depth_pernode.csv', 'EXAMPLE-size_counts.csv', and 'EXAMPLE-depth_counts.csv'")
     parser.add_argument("--debug", action='store_true', help="output debugging info")
     global args
     args = parser.parse_args()
